1f8dba42b86e1d63e6166fdd59767e0e6069e27a
Eugene Vert <eugene.a.vert@gmail.com>
2023-02-13 04:37:23 +0300

    Add speed/effort setting to config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-def jxl_d(effort=7):
+def jxl_d(effort: int = 7):
     return {
         "name": f"jxl_e{effort}_d",
         "cmd": f"cjxl %IMG% -d %Q% -j 0 -e {effort} --patches=0 %OUT%",
@@ -7,24 +7,28 @@ def jxl_d(effort=7):
     }
 
 
-def avif_q(speed=4):
+def avif_q(speed: int = 4):
     return {
         "name": f"avif_s{speed}_q",
-        "cmd": f"avifenc %IMG% --min 0 --max 63 -d 10 -s {speed} -j 8 -a end-usage=q -a cq-level=%Q% -a color:enable-chroma-deltaq=1 -a color:deltaq-mode=3 -a color:aq-mode=1 -a color:qm-min=0 -a tune=ssim %OUT%",
+        "cmd": f"avifenc %IMG% --min 0 --max 63 -d 10 -s {speed} -j 8"
+        " -a end-usage=q -a cq-level=%Q%"
+        " -a color:enable-chroma-deltaq=1 -a color:deltaq-mode=3"
+        " -a color:aq-mode=1 -a color:qm-min=0 -a tune=ssim %OUT%",
         "ext": "avif",
         "qualities": [2, 50],
-        "quality_integer_only": True
+        "quality_integer_only": True,
     }
 
 
-def cavif_q(speed=4):
+def cavif_q(speed: int = 4):
     return {
         "name": f"cavif_s{speed}_Q",
         "cmd": f"cavif -Q %Q% -s {speed} %IMG% -o %OUT% -f",
         "ext": "avif",
-        "qualities": [81, 99],
+        "qualities": [82, 98],
         #
     }
 
-def replace_dot(i):
-  return str(i).replace(".", ",")
+
+def replace_dot(i: float | str):
+    return str(i).replace(".", ",")
